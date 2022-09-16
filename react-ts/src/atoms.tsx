63150eae05aba8a6331e242b1432d2d060a7f006
moonbi <bomrom97@naver.com>
2022-09-17 01:35:53 +0900

    draggable

--- react-ts/src/atoms.tsx
+++ react-ts/src/atoms.tsx
@@ -1,32 +1,18 @@
 import { atom, selector } from "recoil"
 
-export enum Categories {
-  "TO_DO"="TO_DO",
-  "DOING"="DOING",
-  "DONE"="DONE",
-}
-
-export interface IToDo {
-  text:string;
-  id:number;
-  category: Categories;
-};
-
-export const categoryState = atom<Categories>({
-  key:"category",
-  default: Categories.TO_DO,
+export const minuteState = atom ({
+  key:"minutes",
+  default: 0,
 });
 
-export const toDoState = atom<IToDo[]>({
-  key:"toDo",
-  default: [],
-});
-
-export const toDoSelector = selector({
-  key:"toDoSelector",
-  get: ({get}) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo)=> toDo.category === category);
-  }
+export const hourSeletor = selector<number>({
+  key:"hours",
+  get:({get}) => {
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
+  set:({set}, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes);
+  },
 });
