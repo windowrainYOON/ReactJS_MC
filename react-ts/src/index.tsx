66df91e0217278f8d54809232d13339a82bf35c3
moonbi <bomrom97@naver.com>
2022-09-28 20:33:38 +0900

    save

--- react-ts/src/index.tsx
+++ react-ts/src/index.tsx
@@ -61,7 +61,6 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-weight: 300;
     font-family: 'Source Sans Pro', sans-serif;
-    background: linear-gradient(135deg, #e09, #d0e);
     color: black;
     line-height: 1.2;
   }
